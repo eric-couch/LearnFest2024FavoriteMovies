0732fc2aef5cce0ba9ef63fe549c435b7205bd01
Eric Couch <ericcouch@yahoo.com>
2024-11-14 12:08:30 -0600

    remove search button

--- js/moviesearch.js
+++ js/moviesearch.js
@@ -6,28 +6,34 @@ let MoviePoster = "http://img.omdbapi.com/?apikey=86c39163&i=";
 // DOMContentLoaded event listener
 document.addEventListener('DOMContentLoaded', () => {
     console.log("DOM fully loaded and parsed");
-    const button = document.getElementById("searchbtn");
-    button.addEventListener('click', () => {
-        // async await work together
-        (async () => {
-            const movies = await getMovieData();
-            console.log(movies);
-            const favMovieList = document.getElementById("favMovieList");
-            movies.Search.forEach(movie => {
-                console.log(`Movie Title Returned from Search: ${movie.Title}`);
-                BuildMovieCard(movie);
-            });
-        })();
+    const searchGo = document.getElementById("searchTerm");
+    searchGo.addEventListener('keypress', e => {
+        if (e.key === 'Enter') {
+            console.log("Enter key pressed");
 
+            // async await work together
+            (async () => {
+                const movies = await getMovieData();
+                console.log(movies);
+                const favMovieList = document.getElementById("favMovieList");
+                favMovieList.firstElementChild.innerHTML = "";
+                movies.Search.forEach(movie => {
+                    console.log(`Movie Title Returned from Search: ${movie.Title}`);
+                    BuildMovieCard(movie);
+                });
+            })();
+        }
     });
-})
+  });
+
 
 function BuildMovieCard(movie) {
     const { Title, Year, imdbID, Poster } = movie;
     const movieCard = document.getElementById("movieCard");
     let movieCardClone = movieCard.cloneNode(true);
     movieCardClone.querySelector("#moviePoster").src = MoviePoster + imdbID;
-    movieCardClone.querySelector("#cardBody").firstElementChild.innerText = `${Title} (${Year})`;
+    let cardBody = movieCardClone.querySelector("#cardBody");
+    cardBody.firstElementChild.innerText = `${Title} (${Year})`;
     let favButton = movieCardClone.querySelector("#favBtn");
     if (favButton != null) {
         favButton.addEventListener('click', () => {
